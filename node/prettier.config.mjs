5fa78011687d8b45e5e54f4a1d8777a9f32d6d4c
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2023-11-27 17:18:32 +0600

    node project setup

---
+++ node/prettier.config.mjs
@@ -0,0 +1,6 @@
+export default {
+    printWidth: 120,
+    semi: true,
+    tabWidth: 4,
+    trailingComma: "all",
+};
---
+++ node/src/dataset_generator.ts
@@ -0,0 +1,8 @@
+const dir: Record<string, string> = {};
+
+dir.DATA = "../../data";
+dir.ROW = `${dir.DATA_DIR}/row`;
+dir.DATASET = `${dir.DATA_DIR}/dataset`;
+dir.JSON = `${dir.DATASET}/json`;
+dir.IMG = `${dir.DATASET}/img`;
+dir.SAMPLES = `${dir.DATASET}/samples.json`;
